3b5859c94719a5363dc3dea45d29d744ffe2a152
libra <libra@Amazonforest>
2021-03-02 17:59:38 +0530

    first commit

---
+++ demo-jpa-h2/src/main/java/com/learning/demojpah2/DemoJpaH2Application.java
@@ -0,0 +1,13 @@
+package com.learning.demojpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaH2Application.class, args);
+	}
+
+}
---
+++ demo-jpa-h2/src/test/java/com/learning/demojpah2/DemoJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.demojpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
