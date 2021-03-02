3f08eedd134c29cae62cc5ff09bad3a9a4e38ad0
libra <libra@Amazonforest>
2021-03-02 18:39:22 +0530

    added fetchAllStudents in controller

--- demo-jpa-h2/src/main/java/com/learning/demojpah2/controller/StudentController.java
+++ demo-jpa-h2/src/main/java/com/learning/demojpah2/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.learning.demojpah2.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.learning.demojpah2.model.Student;
 import com.learning.demojpah2.repository.StudentRepository;
 
 @Controller
@@ -15,5 +19,15 @@ public class StudentController {
 	@RequestMapping("/")
 	public String home() {
 		return "home.jsp";
+	}
+	
+	@RequestMapping("/fetchAllStudents")
+	public ModelAndView fetchAllStudents() {
+		ModelAndView mv=new ModelAndView("showView.jsp");
+		
+		List<Student> studentList=(List<Student>) studentRepository.findAll();
+		mv.addObject("students", studentList);
+		
+		return mv;
 	}
 }
