53c2ccf963a082bcbcccc002011c1f8325b44035
libra <libra@Amazonforest>
2021-03-03 01:35:46 +0530

    added new rest api using json

--- demo-jpa-h2/src/main/java/com/learning/demojpah2/controller/StudentController.java
+++ demo-jpa-h2/src/main/java/com/learning/demojpah2/controller/StudentController.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.learning.demojpah2.model.Student;
@@ -59,4 +65,26 @@ public class StudentController {
 		
 		return mv;
 	}
+	
+	@RequestMapping("/fetchAllStudentsJson")
+	@ResponseBody
+	public List<Student> fetchAllStudentsJson(){
+		
+		List<Student> studentList=(List<Student>) studentService.fetchAllStudents();
+		return studentList;
+	}
+	
+	@RequestMapping(
+			path = "/addStudentJSON",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces= MediaType.APPLICATION_JSON_VALUE
+			)
+	@ResponseBody
+	public Student addStudentJSON(@RequestBody Student student) {
+		studentService.addStudent(student);
+		return studentService.findStudent(student.getId());
+	}
+	// add header in postman
+	// Content-Type: application/json
 }
